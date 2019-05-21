bb90e06a527fb4eb6f275aa2eac863176f0048fb
alonsohhl <alonso.hl25@gmail.com>
2019-05-20 19:40:46 -0700

    first commit

---
+++ app/index.js
@@ -0,0 +1,17 @@
+require("./main.css");
+// require("./css/animate.css");
+// // require("./css/bootstrap.css");
+// // require("./css/font-awsome.css");
+// require("./css/magnific-popup.css");
+// require("./css/main.css");
+// // require("./css/magnific-popup.css");
+
+
+
+// var component = require("./component"); 
+// var app = document.createElement("div"); 
+
+// document.body.appendChild(app); //agregar e; dov
+
+// app.appendChild(component());// editar div
+
